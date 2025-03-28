8186a2c0f6f4883c790fb8bada14d799686ebe81
zhar <91694359+zh4r@users.noreply.github.com>
2025-03-28 08:10:36 +0200

    base-apparel fix mailValidate script, add user feedback for successful mail submit

--- base-apparel/mailValid.js
+++ base-apparel/mailValid.js
@@ -1,33 +1,36 @@
-const main = document.querySelector('main');
 const body = document.querySelector('body');
+const mainInput = document.getElementById('mainInput');
+const submittedMain = document.getElementById('submittedMain');
 const button = document.querySelector('button');
 const emailInput = document.getElementById('email');
-const submittedMain = document.getElementById('submittedMain');
-let mailSpan = document.getElementById('mailSpan');
-submittedMain.classList.add('hide');
+const errorImg = document.getElementById('errorImg');
+const errMsg = document.getElementById('errMsg');
+const mailSpan = document.getElementById('mailSpan')
+submittedMain.classList.toggle('hide');
 
 function validateEmail() {
     const email = emailInput.value;
-    const mailErr = document.getElementById('email-error');
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
     if (!emailPattern.test(email)) {
         emailInput.classList.add('invalidMail');
-        mailErr.style.display = 'block';
+        errorImg.style.visibility = 'visible';
+        errMsg.style.visibility = 'visible';
         button.disabled = true;
     } else {
         emailInput.classList.remove('invalidMail');
-        mailErr.style.display = 'none';
+        errorImg.style.visibility = 'hidden';
+        errMsg.style.visibility = 'hidden';
         button.disabled = false;
     }
 };
 
 function toggleHide() {
-    const email = document.getElementById('email').value;
+    const email = emailInput.value;
     submittedMain.classList.toggle('hide');
-    main.classList.toggle('hide');
+    mainInput.classList.toggle('hide');
     mailSpan.textContent = email;
-};
+}
 
 button.addEventListener('click', (event) => {
     event.preventDefault();
@@ -36,6 +39,8 @@ button.addEventListener('click', (event) => {
         toggleHide();
     }
 });
+
+
 
 body.addEventListener('keydown', function(event) {
     const dismissBtn = document.getElementById('dismissBtn');
